877d5c72c846c5ebabaeba8ffbf21e23e3e93ffc
senshikikai <senshikikai@tutanota.com>
2022-09-18 12:38:03 -0400

    fix: better xlsx output

--- scraper.py
+++ scraper.py
@@ -4,6 +4,8 @@
 import tabula
 import pandas as pd
 
+def isNaN(num):
+    return num!= num
 
 input_file = "PACKING.pdf"
 output_file = "PACKING.xlsx"
@@ -11,12 +13,11 @@ output_file = "PACKING.xlsx"
 #scrap data from pdf
 dfs = tabula.read_pdf(input_file,pages = 'all',stream=True,guess=False,relative_area=True,area=(40,5,95,60),columns=[160,330],encoding='utf-8-sig')
 
-
-i=0
 for df in dfs:
-	dfs[i].columns = ["sku", "description", "quantity" ]
-	i=i+1
-
+	df.columns = ["sku", "description", "quantity" ]
+	for r, row in df.iterrows():
+		s = row['quantity']
+		if isNaN(s): df.drop(r, inplace = True) #I know is adviced to avoid inplace but it makes more simple
 
 output_df = pd.concat(dfs)
 output_df = output_df.groupby(['sku','description'], as_index=False).sum()
